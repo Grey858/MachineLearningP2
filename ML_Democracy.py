a4eb0973914c1df7e3a08fc3039a1d29f6ba4586
Timothy Flavin <BinaryOwlizard@gmail.com>
2022-02-14 14:56:16 -0600

    Added bagging and made the feature limits more flexible

--- ML_Democracy.py
+++ ML_Democracy.py
@@ -87,15 +87,16 @@ def remove_all_algos():
   global __algos__ 
   __algos__ = list()
 
-def train_algos(output=False, featureProportion=-1.0):
+def train_algos(output=False, featureProportion=-1.0, bag=False):
   if featureProportion>1.0:
     raise ArgumentError("Feature proportion cannot be more than 1.0")
   for i in __algos__: 
     if not i.pre_trained:
-      tx = i.x_train
+      tx = i.x_train 
       ty = i.y_train
       tex = i.x_test
       tey = i.y_test
+
       indices=list()
       if(featureProportion>=0):
         indices = list(random.sample(list(np.arange(0,tx.shape[i.axis])),int(tx.shape[i.axis]*featureProportion)))
@@ -106,17 +107,25 @@ def train_algos(output=False, featureProportion=-1.0):
         i.indices=None
       #print(indices)
       if tx is None:
-        tx = np.take(__default_x_train__, indices,axis=i.axis)
+        tx = __default_x_train__
       if ty is None:
         ty = __default_y_train__
       if tex is None:
-        tex = np.take(__default_x_test__, indices,axis=i.axis)
+        tex = __default_x_test__
       if tey is None :
         tey = __default_y_test__
       
       if(featureProportion>=0):
-        tx = np.take(__default_x_train__, indices,axis=i.axis)
-        tex = np.take(__default_x_test__, indices,axis=i.axis)
+        tx = np.take(tx, indices,axis=i.axis)
+        tex = np.take(tex, indices,axis=i.axis)
+
+      if bag:
+        choices = random.choices(population=list(np.arange(0,len(i.x_train))),k=len(i.x_train))
+        tx = tx[choices]
+        ty = ty[choices]
+        choices = random.choices(population=list(np.arange(0,len(i.x_test))),k=len(i.x_test))
+        tex = tex[choices]
+        tey = tey[choices]
 
       start = time.time()
       i.train_score, i.test_score, i.model = i.init_func(tx, ty, tex, tey)
