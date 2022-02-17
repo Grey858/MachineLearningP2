cc9e08b74f671ba5e858a8485cb23f2da902f743
Timothy Flavin <BinaryOwlizard@gmail.com>
2022-02-16 19:14:37 -0600

    Fixed some bugs and got the tree working on blobs

--- BlobsDriver.py
+++ BlobsDriver.py
@@ -1,25 +1,31 @@
 import pandas as pd
 import numpy as np
-from sklearn.tree import DecisionTreeClassifier
+#from sklearn.tree import DecisionTreeClassifier
+from DecisionTree import dtree
 from dataset import blobs
 
 def get_blobs(tvts = [0.7,0.15,0.15]):
-  data = blobs(300, [np.array([1, 2]), np.array([3, 4])], [np.array([[0.5, 0.5], [0.125, 0.25]])] * 2).to_numpy()
+  data = blobs(200, [np.array([1, 2]), np.array([5, 6])], [np.array([[0.25, 0], [0, 0.25]])] * 2).to_numpy()
   l = data.shape[0]
   tx = data[0:int(l*tvts[0]),:2]
-  ty = data[0:int(l*tvts[0]),2:].astype(int)
+  ty = data[0:int(l*tvts[0]),2:].astype(int).flatten()
   tvx = data[int(l*tvts[0]):int(l*tvts[0])+int(l*tvts[1]),:2]
-  tvy = data[int(l*tvts[0]):int(l*tvts[0])+int(l*tvts[1]),2:].astype(int)
+  tvy = data[int(l*tvts[0]):int(l*tvts[0])+int(l*tvts[1]),2:].astype(int).flatten()
   tex = data[int(l*tvts[1]):int(l*tvts[1])+int(l*tvts[2]),:2]
-  tey = data[int(l*tvts[1]):int(l*tvts[1])+int(l*tvts[2]),2:].astype(int)
+  tey = data[int(l*tvts[1]):int(l*tvts[1])+int(l*tvts[2]),2:].astype(int).flatten()
+
+  #print(tvy)
+  #input("tvy")
+
   return tx, ty, tvx, tvy, tex, tey
 
 
 def dtree_init(x_train, y_train, x_test, y_test):
-  dtree = DecisionTreeClassifier().fit(x_train, y_train)
-  test_score = dtree.score(x_test, y_test)
-  train_score = dtree.score(x_train, y_train)
-  return train_score, test_score, dtree
+  dt = dtree(method="missclassification")
+  dt.fit(x_train, y_train)
+  test_score = dt.score(x=x_test, y=y_test)
+  train_score = dt.score(x=x_train, y=y_train)
+  return train_score, test_score, dt
 def predict(model, x):
   temp = model.predict(x)
   return temp
@@ -47,7 +53,7 @@ for f in fp:
 # could be classified with a member of our hypothesis space
 
 import matplotlib.pyplot as plt
-data = blobs(300, [np.array([1, 2]), np.array([3, 4])], [np.array([[0.5, 0.5], [0.125, 0.25]])] * 2).to_numpy()
+data = blobs(200, [np.array([1, 2]), np.array([5, 6])], [np.array([[0.25, 0], [0, 0.25]])] * 2).to_numpy()
 print(data.shape)
 plt.scatter(x=data[:,0], y=data[:,1], s=5, c=data[:,2])
 plt.show()
